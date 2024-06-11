2ca0f51e45c4dfdd6468891aac55f275b07df9ff
Marius Apostu <apostu_marius_lucian@yahoo.co.uk>
2024-06-11 17:57:12 +0300

    curs28 actionClass

--- src/main/java/selenium/utils/SeleniumWrappers.java
+++ src/main/java/selenium/utils/SeleniumWrappers.java
@@ -5,8 +5,10 @@ import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.TestException;
 
 public class SeleniumWrappers extends BaseTest{
 
@@ -39,5 +41,20 @@ public class SeleniumWrappers extends BaseTest{
 		
 	}
 	
+	public void hoverElement (By locator) {
+		
+		try{
+			WebElement element = driver.findElement(locator);
+			Actions action = new Actions(driver);
+			action.moveToElement(element).perform();
+		}catch(Exception e) {
+			new TestException(e.getMessage());
+			
+		}		
+	}
+	
+	public WebElement getWebElement (By locator) {
+		return driver.findElement(locator);
+	}
 	
 }
