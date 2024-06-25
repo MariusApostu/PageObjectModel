492e1ecb2e07db77d490e317b88fb4d6c0250f60
Marius Apostu <apostu_marius_lucian@yahoo.co.uk>
2024-06-25 18:02:32 +0300

    curs32 window handle

--- src/main/java/tests/WindowHandleExample.java
+++ src/main/java/tests/WindowHandleExample.java
@@ -1,5 +1,9 @@
 package tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import selenium.utils.BaseTest;
@@ -8,14 +12,53 @@ public class WindowHandleExample extends BaseTest {
 	
 	
 	@Test
-	public void windowHandleExample() {
+	public void windowHandleExample() throws InterruptedException {
 		
 		driver.get("https://keybooks.ro/event/seminar-of-modern-art/");
 		
-		
-		driver.switchTo().frame(app.getWebElement(app.genericEvent.mapFrame));
 		app.scrollVertically(900);
+		
+		System.out.println("Current window before tab: " + driver.getWindowHandle());
+		
+		System.out.println("all open windows: " + driver.getWindowHandles());
+		
+		driver.switchTo().frame(app.getWebElement(app.genericEvent.mapFrame));		
 		app.click(app.genericEvent.viewLargerMapLink);
+		
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		
+		System.out.println("------------------------------------------------------------");
+		
+		System.out.println("Current window: " + driver.getWindowHandle());
+		
+		System.out.println("All open windows after tab: " + driver.getWindowHandles());
+		
+		System.out.println("_______________________________________");
+		
+		List<String> browserTabs = new ArrayList<>(driver.getWindowHandles());
+		
+		driver.switchTo().window(browserTabs.get(1));
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		
+		driver.findElement(By.xpath("//span[text()='Accept all']")).click();
+		driver.findElement(By.id("searchboxinput")).clear();
+		driver.findElement(By.id("searchboxinput")).sendKeys("forest");
+		driver.findElement(By.id("searchbox-searchbutton")).click();
+		
+		Thread.sleep(5000);
+		
+		driver.close();
+		Thread.sleep(2000);
+		
+		driver.switchTo().window(browserTabs.get(0));
+		System.out.println("Current window: " + driver.getWindowHandle());
+		
+		System.out.println("All open windows after tab : " + driver.getWindowHandles());
+		driver.switchTo().frame(app.getWebElement(app.genericEvent.mapFrame));
+		app.click(app.genericEvent.viewLargerMapLink);
+		
 		
 	}
 
